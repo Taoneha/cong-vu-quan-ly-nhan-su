c913dad4726fd251a2e836e291aa85225e9b0fce
Taoneha <106001623+Taoneha@users.noreply.github.com>
2026-09-21 23:52:03 +0700

    Connect app to Supabase project

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* Cấu hình cơ sở dữ liệu online dùng chung cho bảng tăng ca. */
 window.CV_SUPABASE_CONFIG = {
-  url: "",
-  publishableKey: ""
+  url: "https://ouynczjpfbemqejawiof.supabase.co",
+  publishableKey: "sb_publishable_664437sVjNHvyKavQX4hSA_enNn79pn"
 };
